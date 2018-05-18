5527802834a9a879487b4333c74f02415a07377f
Irvin <sabzill_ing@naver.com>
2018-05-18 15:57:32 +0900

    E01 Clear
    5.18(금) 15:57

--- TopViewShooting_Youtube/Assets/Scripts/Player.cs
+++ TopViewShooting_Youtube/Assets/Scripts/Player.cs
@@ -6,16 +6,32 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;    // 플레이어 이동속도
+
+    Camera viewCamera;              // 카메라 캐싱
     PlayerController controller;    // PlayerController 스크립트 캐싱
 
 	void Start ()
     {
         controller = GetComponent<PlayerController>();      // Player스크립트와 PlayerController스크립트가 같은 오브젝트에 붙어있음
+        viewCamera = Camera.main;                           // 메인카메라 할당
 	}
 	
 	void Update ()
     {
-        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertiacl"));    // 이동을 입력받음
-        Vector3 moveVelocity = moveInput.normalized * moveSpeed;                                        // 단위백터에 속도를 곱해줌
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));      // 이동을 입력받음 (GetAxisRaw는 기본 스무딩 적용X)
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;                                                // 단위백터에 속도를 곱해줌
+        controller.Move(moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);     // 카메라부터 마우스포지션까지의 Ray 생성
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);        // inNormal은 법선벡터, inPoint는 원점에서의 거리
+                                                                        // 기존 바닥이 아니라 new Plane 을 만들어주는 이유는 마우스로 기존바닥 밖을 가리키면 false 반환
+        float rayDistance;                                              // 카메라에서 플레인에 부딪힌 거리
+
+        if(groundPlane.Raycast(ray, out rayDistance))               // rayDistance가 Raycast 메소드 안에서 할당받은 값으로 변함
+        {
+            Vector3 point = ray.GetPoint(rayDistance);              // GetPoint는 Ray가 부딪힌 좌표를 반환함
+            Debug.DrawLine(ray.origin, point, Color.green);
+            controller.LookAt(point);
+        }
 	}
 }
--- TopViewShooting_Youtube/Assets/Scripts/PlayerController.cs
+++ TopViewShooting_Youtube/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+[RequireComponent (typeof(Rigidbody))]              // PlayerController 스크립트가 있는 오브젝트에는 Rigidbody 컴포넌트가 강제로 붙어있어야함
+public class PlayerController : MonoBehaviour
+{
+    Vector3 velocity;
+    Rigidbody myRigidbody;       // RIgidbody 캐싱
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+    
+    public void LookAt(Vector3 _lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(_lookPoint.x, transform.position.y, _lookPoint.z);       // y좌표를 고정시킨 마우스포인트 좌표
+        transform.LookAt(heightCorrectedPoint);                                                             // heightCorrectedPoint를 바라보게 함
+    }
+
+    public void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);        // 실제 이동값을 받아서 이동시키는 부분
+    }
 }
