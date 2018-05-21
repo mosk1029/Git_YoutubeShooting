395236634d27ba270a2086b4620e119c8aeaf25b
Irvin <sabzill_ing@naver.com>
2018-05-21 15:06:24 +0900

    E03 Clear
    5/21(월) 15:06

---
+++ TopViewShooting_Youtube/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof (NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent pathfinder;        // 에너미 NavMeshAgent 캐싱
+    Transform target;               // 타겟 캐싱
+
+	void Start ()
+    {
+        pathfinder = GetComponent<NavMeshAgent>();  // NavMeshAgent 레퍼런스 가져옴
+        target = GameObject.FindGameObjectWithTag("Player").transform;      // 타겟을 플레이어 태그로 찾음
+
+        StartCoroutine(UpdatePath());
+	}
+	
+	void Update ()
+    {
+	}
+
+    IEnumerator UpdatePath()            // 코루틴 사용하여 경로 업데이트
+    {
+        float refreshRate = 0.25f;
+
+        while (target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0f, target.position.z);
+            pathfinder.SetDestination(targetPosition);
+
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
--- TopViewShooting_Youtube/Assets/Scripts/Projectile.cs
+++ TopViewShooting_Youtube/Assets/Scripts/Projectile.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    float speed = 10f;          // 총알의 속도
+    public LayerMask collisionMask; // 레이어마스크 캐싱
+    float speed = 10f;               // 총알의 속도
 
     public void SetSpeed(float _newSpeed)           // 총알에 속도를 할당해주는 메소드
     {
@@ -13,6 +14,25 @@ public class Projectile : MonoBehaviour
 
 	void Update ()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);      // 총알 발사
-	}   
+        float moveDistance = speed * Time.deltaTime; // 프레임마다 이동할 거리
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);      // 총알 발사
+	}
+
+    void CheckCollisions(float _moveDistance)                     // 충돌을 감지하는 메소드(레이캐스트로 구현한다)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, _moveDistance, collisionMask, QueryTriggerInteraction.Collide))   // QueryTriggerInteraction : 트리거랑 충돌했을때 충돌할것인가 말것인가
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit _hit)       // 충돌했을때 발생하는 메소드
+    {
+        Debug.Log(_hit.collider.name);
+        Destroy(gameObject);
+    }
 }
