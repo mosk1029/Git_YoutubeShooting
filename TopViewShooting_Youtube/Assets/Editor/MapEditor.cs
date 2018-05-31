0391a530d5c7a10ab734aff8089dcf81ea9c90e6
Irvin <sabzill_ing@naver.com>
2018-06-01 01:44:24 +0900

    E08 Clear
    6/1(금) 01:44

---
+++ TopViewShooting_Youtube/Assets/Editor/MapEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]      // 이 에디터 스크립트가 어떤 스크립트를 다루는지 명시
+public class MapEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        MapGenerator map = target as MapGenerator;
+
+        map.GenerateMap();
+    }
+
+}
---
+++ TopViewShooting_Youtube/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public Transform tilePrefab;            // 타일의 프리팹
+    public Vector2 mapSize;                 // 맵사이즈
+
+    [Range(0,1)]        // Range로 범위를 한정
+    public float outlinePercent;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        string holderName = "Generated Map";        // 하이라키상에서 타일들을 가지고 있을(자식으로 가지고 있을) 부모
+        if(transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+
+        for (int i = 0; i < mapSize.x; i++)
+        {
+            for (int j = 0; j < mapSize.y; j++)
+            {
+                Vector3 tilePosition = new Vector3(-mapSize.x * 0.5f + 0.5f + i, 0f, -mapSize.y * 0.5f + 0.5f + j);     // new Vector3 선언을 통해 정중앙에 오게함
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90f));       // quad이므로 x축으로 90도 회전시켜줌
+                newTile.localScale = Vector3.one * (1 - outlinePercent);                                                // 타일의 크기를 줄여 테두리 생성
+                newTile.parent = mapHolder;
+            }
+        }
+    }
+
+}
